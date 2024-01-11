03417600f32f815c946e40852a414f61bcdfddd9
Elmer Balbin <contact@elmer.dev>
2024-01-11 10:15:01 -0800

    first commit

---
+++ exercise.js
@@ -0,0 +1,15 @@
+// Instructions: 10 points
+// 1. Create a superclass called Employee
+// 2. Add a constructor that takes in a name, salary, and hireDate
+// 3. Add a method called introduction that returns name, salary, and hireDate in a sentence
+// 4. Create an instance called employee1 with your name, salary, and hireDate
+// 5. Call the introduction method and output on the console
+// 6. Create a class called Manager that extends the Employee class
+// 7. Using a constructor, create a new property called teamSize. Make sure to call the super method to inherit properties from the Employee class
+// 8. Create a method called teamIntroduction that returns the introduction method from the Employee class, but also adds teamSize to the sentence. Use super to override the introduction method from the Employee class
+// 9. Create an instance of Manager called manager1 with your own name, salary, hireDate, and teamSize
+// 10. Call the teamIntroduction method and output on the console
+
+class Employee {
+
+}
